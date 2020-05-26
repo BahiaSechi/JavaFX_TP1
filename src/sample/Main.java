75e6e5f375b6a4d0965439f651ab45b425e51ca2
BahiaSechi <bahia.s@outlook.fr>
2020-05-26 16:59:02 +0200

    First commit

---
+++ src/sample/Main.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+//https://openclassrooms.com/fr/courses/26832-apprenez-a-programmer-en-java/23832-tp-creez-une-calculatrice
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Calculatrice");
+        primaryStage.setScene(new Scene(root, 320, 450));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

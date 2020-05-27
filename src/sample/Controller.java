2ecb8597ce312f848af8a8b8b6e67d3c1d2bfa67
BahiaSechi <bahia.s@outlook.fr>
2020-05-27 09:03:03 +0200

    Begin Controller + ad fx:id on buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,9 @@
 package sample;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class Controller {
 
     /**
@@ -7,5 +11,14 @@ public class Controller {
      * liste operateurs
      * Mode base
      */
+
+    @FXML
+    private TextField valeur;
+
+    @FXML
+    private Button button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
+
+    @FXML
+    private Button buttonA, buttonB, buttonC, buttonE, buttonF;
 
 }
